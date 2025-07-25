418d23cee1cfa77cf26bc4fc97bf5d2e2f3f77c1
Yaezerrr <ibekweikechukwu2@gmail.com>
2025-07-25 03:07:09 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/yaezerrr/' // Must match your repository name
+  base: '/Yaezerrr/' // Must match your repository name
 });
